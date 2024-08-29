99cf9115b0f9d6f7082430f448ce9c570f5eb8f6
Kingdk88 <143623116+Kingdk88@users.noreply.github.com>
2024-08-29 08:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002141133985))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 7147100550))
+OWNER_ID = int(getenv("OWNER_ID", 7338182362))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Deepking88/HEER",
+    "https://github.com/Kingdk88/SukkuMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FONT_CHANNEL_01")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Exampurrs")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_Alfaz_E_Sukoon_ll")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/love_in_timeee")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
